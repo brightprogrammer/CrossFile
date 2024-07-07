70f68f480a513cf6acd1f4ab9a77115dbd764e3a
Siddharth Mishra <admin@brightprogrammer.in>
2024-07-07 21:29:54 +0530

    Working On Tests
    CMake will now download the tree-sitter github repo and build the tree-sitter
    cli automatically and use that everywhere (not depending on OS installation of
    tree-sitter-cli)
    
    Also tests will now always be executed during the build step.
    
    TODO: dependeny management doesn't look too much appealing to me, need to handle
    that if possible.

--- CMake/TreeSitterMakeAvailable.cmake
+++ CMake/TreeSitterMakeAvailable.cmake
@@ -10,11 +10,21 @@
 # the project.
 # --------------------------------------------------------------------------------------------------
 
+set(TREE_SITTER_SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/Dependencies/tree-sitter)
+set(TREE_SITTER_BUILD_DIR ${TREE_SITTER_SOURCE_DIR}/src/tree-sitter-gh)
+
+set(TREE_SITTER_CLI_EXE ${TREE_SITTER_BUILD_DIR}/target/release/tree-sitter CACHE PATH "tree-sitter cli executable")
+set(TREE_SITTER_INCLUDE_DIR ${TREE_SITTER_BUILD_DIR}/lib/include CACHE PATH "Include directory for Tree-sitter Xfile parser")
+set(TREE_SITTER_LIBRARY_DIR ${TREE_SITTER_XFILE_BUILD_DIR} CACHE PATH "Library directory for Tree-sitter Xfile parser")
+set(TREE_SITTER_LIBRARIES tree-sitter CACHE STRING "Libraries to link with for Tree-sitter Xfile parser")
+set(TREE_SITTER_DEPENDENCIES BuildTreeSitter CACHE STRING "Dependencies to depend on if tree-sitter is to be used")
+
+if(TARGET tree-sitter-gh)
+    return()
+endif()
+
 # Allows us to download remote projects and include them within our project
 include(ExternalProject)
-
-set(TREE_SITTER_SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/Dependencies/tree-sitter)
-set(TREE_SITTER_BUILD_DIR ${CMAKE_CURRENT_BINARY_DIR}/Dependencies/tree-sitter/src/tree-sitter-gh)
 
 message(STATUS "Making tree-sitter available")
 
@@ -28,12 +38,12 @@ ExternalProject_Add(
     LOG_DOWNLOAD 1
     GIT_TAG master
     CONFIGURE_COMMAND ""
-    BUILD_COMMAND make -C ${TREE_SITTER_BUILD_DIR}
+    BUILD_COMMAND make -C ${TREE_SITTER_BUILD_DIR} && cargo build --release --jobs 4 --manifest-path ${TREE_SITTER_BUILD_DIR}/Cargo.toml
     INSTALL_COMMAND ""
 )
 
-message(STATUS "Making tree-sitter available - done")
+add_custom_target(BuildTreeSitter ALL
+    DEPENDS tree-sitter-gh
+)
 
-set(TREE_SITTER_INCLUDE_DIR ${TREE_SITTER_BUILD_DIR}/lib/include CACHE PATH "Include directory for Tree-sitter Xfile parser")
-set(TREE_SITTER_LIBRARY_DIR ${TREE_SITTER_XFILE_BUILD_DIR} CACHE PATH "Library directory for Tree-sitter Xfile parser")
-set(TREE_SITTER_LIBRARIES tree-sitter CACHE STRING "Libraries to link with for Tree-sitter Xfile parser")
+message(STATUS "Making tree-sitter available - done")
--- CMake/TreeSitterXfileGenerate.cmake
+++ CMake/TreeSitterXfileGenerate.cmake
@@ -13,11 +13,12 @@
 # because the generated parser uses Makefile as it's build system.                                            
 # --------------------------------------------------------------------------------------------------
 
-# Make sure tree-sitter is available locally
-include(${PROJECT_SOURCE_DIR}/CMake/TreeSitterMakeAvailable.cmake)
-
-# Required to include Makefile based projects
+include(TreeSitterMakeAvailable)
 include(ExternalProject)
+
+if(TARGET GenerateTreeSitterXfileParser)
+    return()
+endif()
 
 message(STATUS "Generating tree-sitter-xfile parser")
 
@@ -26,6 +27,17 @@ set(TREE_SITTER_XFILE_BUILD_DIR ${CMAKE_CURRENT_BINARY_DIR}/Generated/Xfile)
 set(TREE_SITTER_XFILE_GRAMMAR_JS ${CMAKE_CURRENT_SOURCE_DIR}/grammar.js)
 set(TREE_SITTER_XFILE_BUILD_GRAMMAR_JS ${TREE_SITTER_XFILE_BUILD_DIR}/grammar.js)
 
+set(TREE_SITTER_XFILE_INCLUDE_DIR ${TREE_SITTER_INCLUDE_DIR} ${TREE_SITTER_XFILE_BUILD_DIR}/bindings/c
+    CACHE PATH "Include directory for Tree-sitter Xfile parser")
+set(TREE_SITTER_XFILE_LIBRARY_DIR ${TREE_SITTER_LIBRARY_DIR} ${TREE_SITTER_XFILE_BUILD_DIR}
+    CACHE PATH "Library directory for Tree-sitter Xfile parser")
+set(TREE_SITTER_XFILE_LIBRARIES ${TREE_SITTER_LIBRARIES} tree-sitter-xfile
+    CACHE STRING "Libraries to link with for Tree-sitter Xfile parser")
+set(TREE_SITTER_XFILE_DEPENDENCIES GenerateTreeSitterXfileParser 
+    CACHE STRING "Custom targets on which other targets that use tree-sitter-xfile must depend on")
+set(TREE_SITTER_XFILE_TEST_DEPENDENCIES RunTreeSitterXfileTests 
+    CACHE STRING "Custom targets on which other targets that use tree-sitter-xfile must depend on")
+
 # Create the build directory if it doesn't exist
 file(MAKE_DIRECTORY ${TREE_SITTER_XFILE_BUILD_DIR})
 
@@ -33,21 +45,18 @@ file(MAKE_DIRECTORY ${TREE_SITTER_XFILE_BUILD_DIR})
 file(COPY ${TREE_SITTER_XFILE_GRAMMR_JS} DESTINATION ${TREE_SITTER_XFILE_BUILD_GRAMMAR_JS})
 
 # Run tree-sitter generate command during cmake configure step
-# NOTE: This command assumes that tree-sitter-cli is already installed on host.
-#       If not installed then install using cargo or other preferred methods provided
-#       in official documentation.
-execute_process(
-    COMMAND tree-sitter generate
+add_custom_command(
+    OUTPUT ${TREE_SITTER_XFILE_BUILD_DIR}/tree-sitter-xfile-parser-gen-output
+    COMMAND ${TREE_SITTER_CLI_EXE} generate
     WORKING_DIRECTORY ${TREE_SITTER_XFILE_BUILD_DIR}
-    RESULT_VARIABLE TREE_SITTER_XFILE_GENERATE_RESULT
+    DEPENDS ${TREE_SITTER_DEPENDENCIES}
+    COMMENT "Generating tree-sitter-xfile parser..."
 )
 
-# If tree sitter generation result is not successful.
-if(NOT TREE_SITTER_XFILE_GENERATE_RESULT EQUAL "0")
-    message(FATAL_ERROR "tree-sitter-xfile parser generation failed.")
-else()
-    message(STATUS "Generating tree-sitter-xfile parser - done")
-endif()
+# Add a custom target for the parser generation step
+add_custom_target(GenerateTreeSitterXfileParser ALL
+    DEPENDS ${TREE_SITTER_XFILE_BUILD_DIR}/tree-sitter-xfile-parser-gen-output
+)
 
 # Use ExternalProject to include the generated Makefile project
 ExternalProject_Add(
@@ -57,11 +66,37 @@ ExternalProject_Add(
     CONFIGURE_COMMAND ""
     BUILD_COMMAND make -C ${TREE_SITTER_XFILE_BUILD_DIR}
     INSTALL_COMMAND ""
+    DEPENDS GenerateTreeSitterXfileParser
 )
 
-set(TREE_SITTER_XFILE_INCLUDE_DIR ${TREE_SITTER_INCLUDE_DIR} ${TREE_SITTER_XFILE_BUILD_DIR}/bindings/c
-    CACHE PATH "Include directory for Tree-sitter Xfile parser")
-set(TREE_SITTER_XFILE_LIBRARY_DIR ${TREE_SITTER_LIBRARY_DIR} ${TREE_SITTER_XFILE_BUILD_DIR}
-    CACHE PATH "Library directory for Tree-sitter Xfile parser")
-set(TREE_SITTER_XFILE_LIBRARIES ${TREE_SITTER_LIBRARIES} tree-sitter-xfile
-    CACHE STRING "Libraries to link with for Tree-sitter Xfile parser")
+# Create a custom target for copying test files
+add_custom_target(CopyTestFiles ALL
+    COMMAND ${CMAKE_COMMAND} -E copy_directory
+        ${CMAKE_CURRENT_SOURCE_DIR}/Test/XfileParser
+        ${TREE_SITTER_XFILE_BUILD_DIR}/test/corpus
+    DEPENDS GenerateTreeSitterXfileParser
+    COMMENT "Copying test files to ${TREE_SITTER_XFILE_BUILD_DIR}/test/corpus..."
+)
+
+# Add a custom command to run tree-sitter test
+add_custom_command(
+    OUTPUT ${TREE_SITTER_XFILE_BUILD_DIR}/test/corpus
+    COMMAND ${TREE_SITTER_CLI_EXE} test
+    WORKING_DIRECTORY ${TREE_SITTER_XFILE_BUILD_DIR}
+    DEPENDS CopyTestFiles
+    COMMENT "Running tree-sitter tests..."
+)
+
+# Add a custom target for running tree-sitter tests
+add_custom_target(RunTreeSitterXfileTests ALL
+    DEPENDS ${TREE_SITTER_XFILE_BUILD_DIR}/test/corpus
+)
+
+# Add the test to CTest
+add_test(NAME TreeSitterXfileTest
+    COMMAND ${TREE_SITTER_CLI_EXE} test
+    WORKING_DIRECTORY ${TREE_SITTER_XFILE_BUILD_DIR}
+)
+
+# Make sure the test depends on the parser generation and test file copying
+add_dependencies(RunTreeSitterXfileTests ${TREE_SITTER_DEPENDENCIES} GenerateTreeSitterXfileParser CopyTestFiles)
